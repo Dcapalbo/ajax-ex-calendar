39c225a592893e9d3542ed43ba3a8a45ddd8d028
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-18 16:27:08 +0200

    js-console-handlebars-problem
    ajax-ex-calendar

--- js/main.js
+++ js/main.js
@@ -2,25 +2,28 @@ $(document).ready(function() {
 //Descrizione:
 // Creiamo un calendario dinamico con le festività.
 // Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
-var source = $("#day-template");
-var template = Handlebars.compile(source);
-var context = {
-  "title": "My New Post",
-  "body": "This is my first post!"
-};
-var html = template(context);
-var apiCall = "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0";
-    $.ajax(
-      {
-        "url": apiCall,
-        "method": "GET",
-        "success": function(data, status)
-        console.log(apiCall);
-  },
-      {
-        "error": function (error)
-      },
-    );
+   var date = "2018-01-01";
+   var source = $("#day-template");
+   var template = Handlebars.compile(source);
+   var context = {
+     "day": "",
+     "month": ""
+   };
+   var html = template(context);
+   var apiCall = "https://flynn.boolean.careers/exercises/api/   holidays?year=2018&month=0";
+   $.ajax
+   (
+     {
+       "url": apiCall,
+       "method": "GET",
+       "success": function(data, status) {
+        var results = apiCall.response;
+        console.log(results);
+     },
+       "error": function(request, status, error) {
+         alert("an error has occured");
+     }
+   });
 // Milestone 1
 // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
 

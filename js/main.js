3aea20f258228fee34fcacd2af50501664654dbf
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-18 16:47:45 +0200

    problem-resolved-working-on-milestone1
    ajax-ex-calendar

--- js/main.js
+++ js/main.js
@@ -2,23 +2,35 @@ $(document).ready(function() {
 //Descrizione:
 // Creiamo un calendario dinamico con le festività.
 // Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
+  //make a variable with the initial calendar date
    var date = "2018-01-01";
-   var source = $("#day-template");
+   //make a variable with the current date, using moment script
+   var momentDate = moment(date);
+console.log(momentDate);
+   var source = $("#day-template").html();
    var template = Handlebars.compile(source);
-   var context = {
-     "day": "",
-     "month": ""
-   };
+
+   for (var i = 0; i < momentDate.daysinMonth(); i++) {
+
+     var context = {
+       "day": i,
+       "month": momentDate.format("MMMM"),
+       "fullDate":momentDate.format("YYYY-MM-DD")
+     };
+   }
+
    var html = template(context);
-   var apiCall = "https://flynn.boolean.careers/exercises/api/   holidays?year=2018&month=0";
+   $("#days").append(html);
+   momentDate.add(1, "day");
+
+   var apiCall = "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0";
+
    $.ajax
-   (
-     {
+    ({
        "url": apiCall,
        "method": "GET",
        "success": function(data, status) {
-        var results = apiCall.response;
-        console.log(results);
+        console.log();
      },
        "error": function(request, status, error) {
          alert("an error has occured");
@@ -26,5 +38,4 @@ $(document).ready(function() {
    });
 // Milestone 1
 // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
-
 });

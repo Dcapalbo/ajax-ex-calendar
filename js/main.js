9b324177e03ec82cad4288f9198c77d4568b68af
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-18 16:19:35 +0200

    working-on-ajax-call
    ajax-ex-calendar

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
 //Descrizione:
 // Creiamo un calendario dinamico con le festività.
 // Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
+var source = $("#day-template");
+var template = Handlebars.compile(source);
+var context = {
+  "title": "My New Post",
+  "body": "This is my first post!"
+};
+var html = template(context);
+var apiCall = "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0";
+    $.ajax(
+      {
+        "url": apiCall,
+        "method": "GET",
+        "success": function(data, status)
+        console.log(apiCall);
+  },
+      {
+        "error": function (error)
+      },
+    );
 // Milestone 1
 // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
 

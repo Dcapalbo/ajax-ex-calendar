19e431f97f10982212cffffc5c1c0d2c177aafd9
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-21 21:00:11 +0200

    calendar-done-working-on-festivity-placeholder-and-color-class
    ajax-ex-calendar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   // make a date variable
   var date = "2018-01-01";
   //make moment variable to write a more flexible code
@@ -10,12 +9,9 @@ $(document).ready(function() {
   var momentMonth = momentDate.month();
   var momentYear = momentDate.year();
   var dateComplete = moment(date);
-
   //Handlebars
   var source = $("#day-template").html();
   var template = Handlebars.compile(source)
-
-
     //make an api variable
     apiObject = "https://flynn.boolean.careers/exercises/api/holidays";
     //make the ajax call
@@ -29,6 +25,7 @@ $(document).ready(function() {
     "method": "GET",
     "success": function (data) {
       var holydayData = data.response;
+      console.log(holydayData);
       renderCalendar();
       renderHolyday(holydayData);
     },
@@ -36,27 +33,23 @@ $(document).ready(function() {
       alert("There is an error. "+ err);
     }
 });
-
 // FUNCTIONS
 // Make a function to find the holiday dates and names
   function renderHolyday(holidays) {
    for (var i = 0; i < holidays.length; i++) {
-
      var holidayDate = holidays[i].date;
      console.log(holidayDate);
      var dayOfHoliday = moment(holidayDate).date();
      console.log(dayOfHoliday);
      // Taking back the name of the holiday
      var holidayName = holidays[i].name;
-
      // Apply the color class and write the holiday name inside of the HTML
-     $(".day[data-date=‘" + dayOfHoliday + "’]").addClass("holyday");
-     $(".day[data-date=‘" + dayOfHoliday + "’]").text( " " +   holidayName).addClass("holiday");
+     $(".day[data-date='"+ dayOfHoliday +"']").addClass("holiday");
+     // Scrivo anche il nome della festività.
+     $(".day[data-date='"+ dayOfHoliday +"']").addClass("name_holiday").text(" - " + holidayName);
     }
   };
-
   function renderCalendar() {
-
      // make a cicle "for" to show the numbers of days inside the month
      for (var i = 1; i <= daysInMonth; i++) {
       var context = {
@@ -64,7 +57,6 @@ $(document).ready(function() {
         "month": momentDate.format("MMMM"),
         "dateComplete": dateComplete.format("YYYY-MM-DD")
       };
-
       // compiling the template and append it inside the html
       var html = template(context);
       $("#days").append(html);
@@ -80,8 +72,7 @@ $(document).ready(function() {
           var newDate = momentDate.add(1, "M");
           clear()
           renderCalendar(newDate);
-          renderHolidays(newDate);
-          console.log(renderHolidays);
+          renderHolyday(newDate);
         }
     });
     //make a function which at the click on class prev, will show the previous month
@@ -93,8 +84,7 @@ $(document).ready(function() {
           var newDate = momentDate.subtract(1, "M");
           clear()
           renderCalendar(newDate);
-          renderHolidays(newDate);
-          console.log(renderHolidays);
+          renderHolyday(newDate);
         }
     });
     //make a function to clear the document before to append again the next or previous month's days

bc5ffc45dcd568d3afc257d63938049f76a5a7a3
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-21 15:54:38 +0200

    milestone-1-inserting-color-and-festivity-name
    ajax-ex-calendar

--- js/main.js
+++ js/main.js
@@ -6,27 +6,30 @@ $(document).ready(function() {
    var momentDate = moment(date);
    console.log(momentDate);
    var daysInMonth = momentDate.daysInMonth();
-   console.log(daysInMonth);
-   //make the value of month and year with moment
-   var momentMonth = momentDate.month();
-   console.log(momentMonth);
-   var momentYear = momentDate.year();
 
    //Handlebars
    var source = $("#day-template").html();
    var template = Handlebars.compile(source)
 
+   //make the value of month and year with moment.js
+   var momentMonth = momentDate.month();
+   var momentYear = momentDate.year();
+   var dateComplete = moment(date);
+
    // make a cicle for to show the numbers of days inside the month
    for (var i = 1; i <= daysInMonth; i++) {
     var context = {
       "day": i,
-      "month": momentDate.format("MMMM")
+      "month": momentDate.format("MMMM"),
+      "dateComplete": dateComplete.format("YYYY-MM-DD")
     };
 
     // compiling the template and append it inside the html
     var html = template(context);
     $("#days").append(html);
+    dateComplete.add(1, "days");
   }
+
     //make an api variable
     apiObject = "https://flynn.boolean.careers/exercises/api/holidays";
     //make the ajax call
@@ -52,16 +55,15 @@ $(document).ready(function() {
 // make a function to find the holiday dates and names
   function printHolyday(holidays) {
    for (var i = 0; i < holidays.length; i++) {
+
      var holidayDate = holidays[i].date;
-     console.log(holidayDate);
      var dayOfHoliday = moment(holidayDate).date();
-     console.log(dayOfHoliday);
      // taking back the name of the holiday
      var holidayName = holidays[i].name;
-     console.log(holidayName);
+
      // apply the color class and write the holiday name inside of the HTML
-     $(".day:nth-child("+dayOfHoliday+")").addClass("holydays");
-     $(".day:nth-child("+dayOfHoliday+")").children(".name_holiday").text(" " +   holidayName);
+     $(".day[data-date=‘" + dayOfHoliday + "’]").addClass("holydays");
+     $(".day[data-date=‘" + dayOfHoliday + "’] span").text( " " +   holidayName);
     }
   }
 });
